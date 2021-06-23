b1857dc8fd502cf771d7902666d6d6d6dbe07be2
Chetan Khanna <chetankhanna767@gmail.com>
2021-06-24 02:54:37 +0530

    local-settings

--- coden/settings.py
+++ coden/settings.py
@@ -188,3 +188,10 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'livecoden@gmail.com'
 EMAIL_HOST_PASSWORD = 'eejxcvioadyydfro'
+
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
