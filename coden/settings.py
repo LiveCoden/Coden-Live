60db4a2171b81b5198ce20e4d64676cb6359bbdd
Chetan Khanna <chetankhanna767@gmail.com>
2021-06-24 19:12:47 +0530

    check

--- coden/settings.py
+++ coden/settings.py
@@ -21,14 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qt(of6!#ta8#wf-x$nyh%98@a+g5xg4a89!37(oq&-iq4nf#6o'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -79,20 +71,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'coden.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'coden',
-#         'USER': 'chetankhanna',
-#         'PASSWORD': 'codeit',
-#         'HOST': 'localhost'
-#     }
-# }
 
 
 
