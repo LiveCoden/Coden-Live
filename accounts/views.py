1f3c4c4a893d19d5f60f92e3d8dbd378266ffb02
Chetan Khanna <chetankhanna767@gmail.com>
2021-07-03 02:31:13 +0530

    fix

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password,password_validators_help_texts
 def register(request):
 
-   
+    context ={
+    }
     if request.method == 'POST':
 
         context ={
