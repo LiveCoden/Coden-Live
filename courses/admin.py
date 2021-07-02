146818c39ddef7815d604528e2c036c726b5b2d8
Chetan Khanna <chetankhanna767@gmail.com>
2021-07-03 00:10:28 +0530

    check

--- courses/admin.py
+++ courses/admin.py
@@ -3,8 +3,8 @@ from .models import Course
 # Register your models here.
 
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'icon', 'description')
-    list_display_links = ('id', 'name', 'url_name')
+    list_display = ('id', 'name', 'icon', 'url_name')
+    list_display_links = ('id', )
     list_editable = ('name' ,'url_name')
     search_fields = ('name',)
     list_per_page = 20
