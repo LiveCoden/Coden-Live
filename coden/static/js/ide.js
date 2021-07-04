c55cd68a82358122679a33fd2f5cb375016c8f9b
Chetan Khanna <chetankhanna767@gmail.com>
2021-07-05 01:50:21 +0530

    compiler

--- coden/static/js/ide.js
+++ coden/static/js/ide.js
@@ -740,11 +740,10 @@ var bashSource = "echo \"hello, world\"";
 var basicSource = "PRINT \"hello, world\"";
 
 var cSource = "\
-// Powered by Judge0\n\
 #include <stdio.h>\n\
 \n\
 int main(void) {\n\
-    printf(\"Hello Judge0!\\n\");\n\
+    printf(\"Hello Coden Live!\\n\");\n\
     return 0;\n\
 }\n\
 ";
