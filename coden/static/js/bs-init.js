890016ee6ef8fd4c193d79b11e193c35ac744dbe
Chetan Khanna <chetankhanna767@gmail.com>
2021-07-05 13:12:19 +0530

    fix

--- coden/static/js/bs-init.js
+++ coden/static/js/bs-init.js
@@ -24,72 +24,72 @@ setTimeout(function() {
 
   
 
-  $("#features").attr("data-aos","flip-down")
+//   $("#features").attr("data-aos","flip-down")
 
-// course accordian animation fix
-let ca = document.getElementsByClassName('course-accordian')
-let cae = ca[ca.length-1]
+// // course accordian animation fix
+// let ca = document.getElementsByClassName('course-accordian')
+// let cae = ca[ca.length-1]
 
-console.log(cae)
-  $('#accordion-2').on('hidden.bs.collapse', function (e) {
-	if(e.target == cae)
-	{
-		// $("#course").attr("data-aos","")
+// console.log(cae)
+//   $('#accordion-2').on('hidden.bs.collapse', function (e) {
+// 	if(e.target == cae)
+// 	{
+// 		// $("#course").attr("data-aos","")
 
-		$("#features").attr("data-aos","")
-		$("#footer").attr("data-aos","")
+// 		$("#features").attr("data-aos","")
+// 		$("#footer").attr("data-aos","")
 		
-		$('#features').show()
-		$('#footer').show()
+// 		$('#features').show()
+// 		$('#footer').show()
 		
-		// $("#features").attr("data-aos","flip-down")
-		// $("#footer").attr("data-aos","flip-down")
+// 		// $("#features").attr("data-aos","flip-down")
+// 		// $("#footer").attr("data-aos","flip-down")
 
-		console.log($('#cae.id'))
-	}
-})
+// 		console.log($('#cae.id'))
+// 	}
+// })
 
-console.log($('#cae.id'))
+// console.log($('#cae.id'))
 
-$('html, body').animate({
-	scrollTop: $(cae).offset().top
-  }, 800,)
+// $('html, body').animate({
+// 	scrollTop: $(cae).offset().top
+//   }, 800,)
 
-$('#accordion-2').on('shown.bs.collapse', function (e) {
-	if(e.target == cae)
-	{
-		// $("#features").attr("data-aos","flip-down")
-		// $("#footer").attr("data-aos","")
-		// $('#features').show()
-		// $('#footer').show()
+// $('#accordion-2').on('shown.bs.collapse', function (e) {
+// 	if(e.target == cae)
+// 	{
+// 		// $("#features").attr("data-aos","flip-down")
+// 		// $("#footer").attr("data-aos","")
+// 		// $('#features').show()
+// 		// $('#footer').show()
 		
-		// $("#features").attr("data-aos","flip-down")
-		// $("#footer").attr("data-aos","flip-down")
-		// $('html, body').animate({
-		// 	scrollTop: $(cae).offset().top
-		//   }, 0,)
+// 		// $("#features").attr("data-aos","flip-down")
+// 		// $("#footer").attr("data-aos","flip-down")
+// 		// $('html, body').animate({
+// 		// 	scrollTop: $(cae).offset().top
+// 		//   }, 0,)
 		
-		console.log($('#features'))
-	}
-})
+// 		console.log($('#features'))
+// 	}
+// })
 
 
-// main accodian faq fix 
+// // main accodian faq fix 
 
-let ca0 = document.getElementsByClassName('main-accordian')
-let cae0 = ca0[ca0.length-1]
-console.log(cae)
-  $('#accordion-1').on('hidden.bs.collapse', function (e) {
-	if(e.target == cae0)
-	{
-		// $("#course").attr("data-aos","")
+// let ca0 = document.getElementsByClassName('main-accordian')
+// let cae0 = ca0[ca0.length-1]
+// console.log(cae)
+//   $('#accordion-1').on('hidden.bs.collapse', function (e) {
+// 	if(e.target == cae0)
+// 	{
+// 		// $("#course").attr("data-aos","")
 
-		$("#footer").attr("data-aos","")
+// 		$("#footer").attr("data-aos","")
 		
-		$('#footer').show()
+// 		$('#footer').show()
 		
-		// $("#features").attr("data-aos","flip-down")
-		// $("#footer").attr("data-aos","flip-down")
+// 		// $("#features").attr("data-aos","flip-down")
+// 		// $("#footer").attr("data-aos","flip-down")
 
-	}
-})
+// 	}
+// })
