390c36d11acd255db0c391e76620eb965dfad07f
Chetan Khanna <chetankhanna767@gmail.com>
2021-07-07 19:13:34 +0530

    mail

--- demoform/views.py
+++ demoform/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
 from .models import DemoForm
 from django.contrib import messages
 from django.core.mail import send_mail
+from django.utils.html import strip_tags
+
 
 def index(request):
 
@@ -10,19 +13,26 @@ def index(request):
         email = request.POST['email']
         course = request.POST['course']
         phone = request.POST['phone']
-        id = request.POST['id']
+        url_name = request.POST['url_name']
 
-        demo = DemoForm(name=name,email=email,course=course,phone=phone)
+        demo = DemoForm(name=name, email=email, course=course, phone=phone)
         demo.save()
+
+        subject = 'Subject'
+        html_message = render_to_string('mail.html', context={"course":course,"name":name})
+        plain_message = strip_tags(html_message)
+   
+
         send_mail(
-        'Subject here',
-        'Here is the message.',
-        'livecoden@gmail.com',
-        ['chetankhanna767@gmail.com'],
-        fail_silently=False,
-)
+           subject= 'Subject here',
+            html_message= html_message,
+            message=plain_message,
+            from_email= 'livecoden@gmail.com',
+            recipient_list= [email],
+            fail_silently=False,
+        )
 
-        messages.success(request, " We will get back to you, Your request has been submitted.")
-       
+        messages.success(
+            request, " We will get back to you, Your request has been submitted.")
 
-    return redirect('/courses/' + id)
+    return redirect('/courses/' + url_name )
