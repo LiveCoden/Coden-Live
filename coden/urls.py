8ef78f67485ea60e48cc06ddbfe3cb7e7f808423
Chetan Khanna <chetankhanna767@gmail.com>
2021-07-11 04:49:09 +0530

    sitemap

--- coden/urls.py
+++ coden/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
+from django.views.generic.base import TemplateView
 
 from landing.sitemaps import StaticViewSiteMap
 from courses.sitemaps import StaticViewSiteMap as courses
@@ -24,6 +25,6 @@ urlpatterns = [
     path('', include('landing.urls',),),
     path('courses/', include('courses.urls')),
     path('dencod/', admin.site.urls), 
-    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
-     name='django.contrib.sitemaps.views.sitemap')  
+#     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+#      name='django.contrib.sitemaps.views.sitemap')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
