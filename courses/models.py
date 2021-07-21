c9d708df6fc13c6d55c86fef8a159f92f2345434
Chetan Khanna <chetankhanna767@gmail.com>
2021-07-21 18:14:08 +0530

    fix

--- courses/models.py
+++ courses/models.py
@@ -4,7 +4,7 @@ from django.db.models.lookups import IContains
 class Course(models.Model):
     name = models.CharField(max_length=100)
     icon = models.ImageField(upload_to='photos/%Y/%m/%d')
-    description = models.CharField(max_length=200)
+    description = models.TextField()
     content = models.JSONField(blank=True)
     amount = models.IntegerField(default=0)
     priority = models.IntegerField(default=1)
