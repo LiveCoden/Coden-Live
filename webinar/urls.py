2b6f453803947b64650f3f11f30e996f810ffebb
Chetan Khanna <chetankhanna767@gmail.com>
2021-08-14 02:14:50 +0530

    now

--- webinar/urls.py
+++ webinar/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('frontend', views.frontend, name='web_frontend'),
+    path('frontend/', views.frontend, name='web_frontend'),
     path('frontend/post', views.frontend_post, name='web_frontend_post'),
 
 
