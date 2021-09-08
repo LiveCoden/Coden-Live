44c704e279f1a4a294f6545e670f7296ac245c60
Chetan Khanna <chetankhanna767@gmail.com>
2021-09-09 01:15:20 +0530

    link added

--- webinar/views.py
+++ webinar/views.py
@@ -9,7 +9,7 @@ worksheet = sh.sheet1
 
 def webinar(request):
    
-    return redirect('https://google.com')
+    return redirect('https://docs.google.com/forms/d/e/1FAIpQLSfbBtmllLAMtXsI3Bi-e9SrrFAX1JOnqXsVPAaBYYle5Ujq_w/viewform')
  
 
    
