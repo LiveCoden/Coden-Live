e2c2c4be3b241fd71ffcaa5cb5e7a0aa4efe249d
Chetan Khanna <chetankhanna767@gmail.com>
2021-10-12 00:09:19 +0530

    addded endpoint

--- webinar/views.py
+++ webinar/views.py
@@ -1,16 +1,19 @@
 from django.contrib import messages
 from django.http.response import JsonResponse
 from django.shortcuts import redirect, render
+from django.views.decorators.csrf import csrf_exempt
 
 
+@csrf_exempt
 def webinar(request):
 
     if(request.method) == 'POST':
-        code = request.POST['code']
-        if(code!=None):
-            return JsonResponse("check : correct")
+        code = request.GET["code"]
+        print(code)
+        if(code=="748"):
+            return JsonResponse("{check : yes}", safe=False)
         else:
-            return JsonResponse("check : incorrect")
+            return JsonResponse("{check : no}", safe=False)
 
    
     return redirect('https://docs.google.com/forms/d/e/1FAIpQLSfbBtmllLAMtXsI3Bi-e9SrrFAX1JOnqXsVPAaBYYle5Ujq_w/viewform')
