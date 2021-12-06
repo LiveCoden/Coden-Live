d83002967c9352756b52c49aa63010686d448341
Chetan Khanna <chetankhanna767@gmail.com>
2021-12-06 13:01:04 +0530

    robots.txt added

--- services/urls.py
+++ services/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='services_main'),
+        path('robots.txt', views.robot)  ,
+
     path('contact_post', views.contact_post, name='contact_post'),
 
 ]
--- services/views.py
+++ services/views.py
@@ -30,3 +30,5 @@ def contact_post(request):
       
    return redirect('services_main')
 
+def robot(request):
+    return render(request,'robots.txt',content_type='text')    
